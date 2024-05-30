4fec7256d9eeaf778549cd785d572924461f3daa
mikewon98a <mike@aquilaict.com>
2024-05-30 11:58:55 +0300

    second commit

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+app.get('/',()=>{
+    res.send("Hello World")
+})
+
 app.listen(8080,(req,res)=>{
     console.log("Server is running")
 })
